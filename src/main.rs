67294c592067e1a70ed68afa77c52c61de8e1973
Tony Lykke <tony@roganartu.com>
2021-07-03 19:28:05 -0400

    add read-only and no-raw flags

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     //   - config file path
     //   - redis url(s)
     //   - sentinel mode
-    //   - read-only mode
     let matches = App::new("fusedis")
         .version(crate_version!())
         .author("Tony Lykke")
@@ -18,20 +17,37 @@ fn main() {
             Arg::with_name("MOUNT_POINT")
                 .required(true)
                 .index(1)
-                .help("Act as a client, and mount FUSE at given path"),
+                .help("Path to mount fusedis at."),
         )
         .arg(
             Arg::with_name("allow-root")
                 .long("allow-root")
-                .help("Allow root user to access filesystem"),
+                .help("Allow root user to access filesystem."),
+        )
+        .arg(
+            Arg::with_name("read-only")
+                .long("read-only")
+                .help("Mount FS in read-only mode. Implies --no-raw."),
+        )
+        .arg(
+            Arg::with_name("no-raw")
+                .long("no-raw")
+                .help("Don't mount /raw path that accepts raw Redis commands."),
         )
         .get_matches();
     env_logger::init();
     let mountpoint = matches.value_of("MOUNT_POINT").unwrap();
-    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    let mut options = vec![MountOption::FSName("hello".to_string())];
     options.push(MountOption::AutoUnmount);
     if matches.is_present("allow-root") {
         options.push(MountOption::AllowRoot);
     }
+    if matches.is_present("read-only") {
+        options.push(MountOption::RO);
+        // TODO set --no-raw
+    } else {
+        options.push(MountOption::RW);
+    }
+    // TODO handle --no-raw
     fuser::mount2(fs::HelloFS, mountpoint, &options).unwrap();
 }
